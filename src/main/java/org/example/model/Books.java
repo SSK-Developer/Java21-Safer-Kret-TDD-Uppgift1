9aaca94e63df260b07553a47d715d7853698ec7b
SSK-Developer <Saferkret@hotmail.com>
2022-11-30 11:18:34 +0100

    TDD uppgift1 update

--- src/main/java/org/example/model/Books.java
+++ src/main/java/org/example/model/Books.java
@@ -1,23 +1,12 @@
 package org.example.model;
 
-import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.cloud.firestore.QuerySnapshot;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
+
 
 public class Books {
 
     public final ArrayList<Book> books;
-
 
     public Books() {
         books = new ArrayList<>();
@@ -29,9 +18,5 @@ public class Books {
 
     public List<Book> getBooks() {
         return books;
-    }
-
-    public void addBook(Book book){
-        books.add(book);
     }
 }
