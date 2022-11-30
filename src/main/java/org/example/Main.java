e301fc7725adc9aca26627d54154c46b691491ca
SSK-Developer <Saferkret@hotmail.com>
2022-11-30 11:17:17 +0100

    TDD uppgift1 update main Method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,5 @@
 package org.example;
 
-import org.example.model.Book;
-import org.example.model.Books;
-import org.example.service.BookingService;
-import org.example.service.PaymentService;
-
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
 
